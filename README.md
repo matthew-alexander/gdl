136c54bfd57cf2a926a75685fd1dfb69091649d4
matthew-alexander <mda54092@gmail.com>
2016-11-09 15:18:58 -0600

    gdl initial'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gdl
 grokking deep learning 
+
---
+++ start.py
@@ -0,0 +1,11 @@
+"""
+high level support for doing this and that.
+"""
+
+knob_weight = 0.5
+input = 0.5
+
+prediction = input * knob_weight
+
+print(prediction)
+
