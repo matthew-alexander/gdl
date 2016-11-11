275e638c8159ede2484cacd76a4dd3ea508ab141
matthew-alexander <mda54092@gmail.com>
2016-11-10 23:21:54 -0600

    error

--- start.py
+++ start.py
@@ -5,9 +5,14 @@ high level support for doing this and that.
 knob_weight = 0.5
 input = 0.5
 
+goal_prediction = 0.8 
+
 prediction = input * knob_weight
 
-print(prediction)
+error = (prediction - goal_prediction)**2
 
-print("hey")
+
+print(prediction)
+print("the error is: ", error)
+
 
