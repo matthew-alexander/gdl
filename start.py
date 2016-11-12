15431cde949d88a29cf9ab85fffdd0c81a217866
matthew-alexander <mda54092@gmail.com>
2016-11-11 23:15:19 -0600

    gradient descent

--- start.py
+++ start.py
@@ -9,20 +9,15 @@ goal_prediction = 0.8
 
 step_amount = 0.001
 
-for iteration in range(1102):
+for iteration in range(50):
     prediction = input *knob_weight
-    error = (prediction - goal_prediction) ** 2 
+    # error = (goal_prediction-prediction) ** 2 
+    error = (goal_prediction - (input * knob_weight)) ** 2
+
+    direction_and_amount = (goal_prediction - prediction) * input
+
+    knob_weight = knob_weight + direction_and_amount
 
     print("Iteration: " + str(iteration) + " Error: " + str(error) + " Prediction: " + str(prediction))
 
-    up_prediction = input * (knob_weight + step_amount)
-    up_error = (goal_prediction - up_prediction) ** 2
-
-    down_predicition = input * (knob_weight - step_amount)
-    down_error = (goal_prediction - down_predicition) ** 2
-
-    if(down_error< up_error):
-        knob_weight = knob_weight - step_amount
-    
-    if(down_error > up_error):
-        knob_weight = knob_weight + step_amount
+  
