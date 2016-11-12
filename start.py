bce8703f21920cff60f863bf7dd2e9ffaaa74539
matthew-alexander <mda54092@gmail.com>
2016-11-11 22:23:48 -0600

    hot and cold

--- start.py
+++ start.py
@@ -7,12 +7,22 @@ input = 0.5
 
 goal_prediction = 0.8 
 
-prediction = input * knob_weight
+step_amount = 0.001
 
-error = (prediction - goal_prediction)**2
+for iteration in range(1102):
+    prediction = input *knob_weight
+    error = (prediction - goal_prediction) ** 2 
 
+    print("Iteration: " + str(iteration) + " Error: " + str(error) + " Prediction: " + str(prediction))
 
-print(prediction)
-print("the error is: ", error)
+    up_prediction = input * (knob_weight + step_amount)
+    up_error = (goal_prediction - up_prediction) ** 2
 
+    down_predicition = input * (knob_weight - step_amount)
+    down_error = (goal_prediction - down_predicition) ** 2
 
+    if(down_error< up_error):
+        knob_weight = knob_weight - step_amount
+    
+    if(down_error > up_error):
+        knob_weight = knob_weight + step_amount
